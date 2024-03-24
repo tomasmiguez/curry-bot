580c62ca0b7088cf493978c6cac7337f8fd3bd7a
Tomas Miguez <tomasmiguez99@gmail.com>
2024-03-24 20:04:32 -0300

    feat: add function to get connection string

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib (getConnString) where
+
+import Data.Yaml (decodeFileEither, ParseException)
+import  Data.Text (Text)
+import Data.Map
+
+type Config = Map Text (Map Text String)
+
+getConnString :: IO (Either ParseException String)
+getConnString = do
+  parsedFile <- decodeFileEither "config/config.yaml" :: IO (Either ParseException Config)
+  case parsedFile of
+    Left x             -> return (Left x)
+    Right parsedConfig -> do
+      let dbConfig = parsedConfig ! "db"
+
+          user = dbConfig ! "user"
+          host = dbConfig ! "host"
+          port = dbConfig ! "port"
+          database = dbConfig ! "database"
+
+          connString = foldr1 (++) ["postgresql://", user, "@", host, ":", port, "/", database]
+      return (Right connString)
