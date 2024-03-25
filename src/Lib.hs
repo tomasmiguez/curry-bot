7039d6005e4fa7b92c7c0298f5a4bc2c54d14ab3
Tomas Miguez <tomasmiguez99@gmail.com>
2024-03-24 23:15:09 -0300

    refactor: more idiomatic Haskell

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,24 +2,21 @@
 
 module Lib (getConnString) where
 
-import Data.Yaml (decodeFileEither, ParseException)
-import  Data.Text (Text)
 import Data.Map
+import Data.Text (Text)
+import Data.Yaml (ParseException, decodeFileEither)
 
 type Config = Map Text (Map Text String)
 
 getConnString :: IO (Either ParseException String)
 getConnString = do
   parsedFile <- decodeFileEither "config/config.yaml" :: IO (Either ParseException Config)
-  case parsedFile of
-    Left x             -> return (Left x)
-    Right parsedConfig -> do
-      let dbConfig = parsedConfig ! "db"
+  return $ buildConnString . (! "db") <$> parsedFile
 
-          user = dbConfig ! "user"
-          host = dbConfig ! "host"
-          port = dbConfig ! "port"
-          database = dbConfig ! "database"
-
-          connString = foldr1 (++) ["postgresql://", user, "@", host, ":", port, "/", database]
-      return (Right connString)
+buildConnString :: Map Text String -> String
+buildConnString config = foldr1 (++) ["postgresql://", user, "@", host, ":", port, "/", database]
+  where
+    user = config ! "user"
+    host = config ! "host"
+    port = config ! "port"
+    database = config ! "database"
