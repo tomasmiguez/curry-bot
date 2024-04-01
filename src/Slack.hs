678a7015bc8328f7d4e9056583410b3262b93ae9
Tomas Miguez <tomasmiguez99@gmail.com>
2024-04-01 15:01:35 -0300

    refactor(slack): extract slackGet

--- src/Slack.hs
+++ src/Slack.hs
@@ -4,13 +4,15 @@ module Slack (listChannels) where
 
 import Data.Aeson
 import Network.HTTP.Simple
+import Config (getSlackToken)
+import Data.String (IsString(fromString))
 
 type ChannelId = String
 type ChannelName = String
 
 data Channel = Channel
-  { id :: ChannelId
-  , name :: ChannelName
+  { id :: !ChannelId
+  , name :: !ChannelName
   } deriving (Show, Eq)
 
 instance FromJSON Channel where
@@ -26,16 +28,17 @@ instance FromJSON ChannelsResponse where
   parseJSON = withObject "ChannelsResponse" $ \obj ->
     ChannelsResponse <$> obj .: "channels"
 
+slackGet :: String -> String -> Request
+slackGet token resource = setRequestBearerAuth (fromString token)
+                        $ setRequestMethod "GET"
+                        $ setRequestPath (fromString $ "api/" ++ resource)
+                        $ setRequestHost "slack.com"
+                        $ defaultRequest
+
 listChannels :: IO (Maybe [Channel])
-listChannels = do
-  let request = setRequestBearerAuth "XXX"
-              $ setRequestMethod "GET"
-              $ setRequestPath "api/users.conversations"
-              $ setRequestHost "slack.com"
-              $ defaultRequest
-
-  response <- httpJSON request
-
-  let maybeResponseBody = getResponseBody response :: Maybe ChannelsResponse
-
-  return $ channels <$> maybeResponseBody
+listChannels = getSlackToken >>= either (const $ return Nothing) listChannelsWithToken
+  where
+    listChannelsWithToken token = do
+      response <- httpJSON $ slackGet token "users.conversations"
+      let maybeResponseBody = getResponseBody response :: Maybe ChannelsResponse
+      return $ channels <$> maybeResponseBody
