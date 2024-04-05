dab63a11b4157b15e8a0313467f95cd07ccf2386
Tomas Miguez <tomasmiguez99@gmail.com>
2024-04-04 21:02:39 -0300

    feat(cli): implement commands

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,26 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+
+import CurryBot (sendTodayBirthdayReminder, updateSlackIds, updateEmployees)
+
 main :: IO ()
-main = undefined
+main = run . head =<< getArgs
+
+run :: String -> IO ()
+run "import-bamboo"   = updateEmployees
+run "update-slack"    = updateSlackIds
+run "update-people"   = do
+  _ <- updateEmployees
+  _ <- updateSlackIds
+  return ()
+run "remind-birthday" = sendTodayBirthdayReminder
+run _                 = putStrLn usage
+
+usage :: String
+usage = "Usage: tech-talk-bot-exe <command>.\n\
+        \Possible commands are:\n\
+        \import-bamboo: Queries Bamboo for people and upserts the list.\n\
+        \update-slack: Updates Slack IDs of already existing people.\n\
+        \update-people: Both previous commands.\n\
+        \remind-birthday: Sends a birthday reminder for the current day."
